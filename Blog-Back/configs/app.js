c13a3227359fa49991a34da2665aade05e4463c5
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-10 17:32:57 -0600

    Se agrega estructura del Back

---
+++ Blog-Back/configs/app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+import postRoutes from '../src/post/post.routes.js'
+
+
+const configs = (app)=>{
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+const routes = (app)=>{
+    app.use('/v1/post', postRoutes)
+}
+
+export const initServer = async()=>{
+    const app = express()
+    try {   
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
+    } catch (err) {
+        console.error('Server init failed', err)
+    }
+}
