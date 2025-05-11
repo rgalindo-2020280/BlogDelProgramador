5ae5ac0b5e5b6180f72ef4722abbbed7a79dac73
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-10 19:20:07 -0600

    Se agregan las rutas y se crean las publicaciones al correr el back

--- Blog-Back/configs/app.js
+++ Blog-Back/configs/app.js
@@ -1,11 +1,11 @@
 'use strict'
-
+import Post from '../src/post/post.model.js'
 import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
 import cors from 'cors'
 import postRoutes from '../src/post/post.routes.js'
-
+import commentRoutes from '../src/coments/comments.routes.js'
 
 const configs = (app)=>{
     app.use(express.json())
@@ -17,7 +17,45 @@ const configs = (app)=>{
 
 const routes = (app)=>{
     app.use('/v1/post', postRoutes)
+    app.use('/v1/comment', commentRoutes)
 }
+
+const createPosts = async () => {
+    try {
+        const postData = [
+            {
+                title: "Curso de React: Introducción",
+                description: "Este es el primer módulo del curso de React, donde aprenderás lo básico sobre este framework.",
+                course: "Taller",
+                createdAt: new Date()
+            },
+            {
+                title: "Curso de Node.js: Backend avanzado",
+                description: "En este curso profundizaremos en el desarrollo de aplicaciones backend con Node.js.",
+                course: "Taller",
+                createdAt: new Date()
+            },
+            {
+                title: "Curso de MongoDB: Base de datos NoSQL",
+                description: "Aprende todo sobre MongoDB y cómo utilizar esta base de datos NoSQL en aplicaciones modernas.",
+                course: "Tecnología",
+                createdAt: new Date()
+            }
+        ]
+
+        const existingPosts = await Post.find()
+        if (existingPosts.length === 0) {
+            await Post.insertMany(postData)
+            console.log("Post created correctly")
+        } else {
+            console.log("Posts already exist in the database")
+        }
+    } catch (error) {
+        console.error("Error when creating posts:", error)
+    }
+}
+
+createPosts()
 
 export const initServer = async()=>{
     const app = express()
