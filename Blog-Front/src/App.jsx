267ed2e61a687ba1f26dbe737c88d23ed07db9b5
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-11 21:20:43 -0600

    Se agrega el contenido para mostrar las publicaciones

---
+++ Blog-Front/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PostList from './components/PostList'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Blog de Aprendizaje</h1>
+            <PostList />
+        </div>
+    )
+}
+
+export default App
