f44e083f69953cfc0f0996626e40e4ee9d1178c5
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-11 21:59:00 -0600

    Se arregla el filtrado por año

--- Blog-Front/src/shared/hooks/usePost.jsx
+++ Blog-Front/src/shared/hooks/usePost.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getPosts, getPostsByCourse, getPostsByYear } from '../../services/api.js'
 
-const usePosts = (course = '', year = '') => { 
+const usePosts = (course = '', year = '') => {
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
@@ -10,14 +10,19 @@ const usePosts = (course = '', year = '') => {
         const fetchPosts = async () => {
             try {
                 setLoading(true)
-                let data;
-                if (year) {
-                    data = await getPostsByYear(year)
+                let data = []
+                if (course && year) {
+                    const coursePosts = await getPostsByCourse(course)
+                    const yearPosts = await getPostsByYear(year)
+                    data = coursePosts.filter(post => yearPosts.some(yPost => yPost._id === post._id))
                 } else if (course) {
                     data = await getPostsByCourse(course)
+                } else if (year) {
+                    data = await getPostsByYear(year)
                 } else {
                     data = await getPosts()
                 }
+
                 setPosts(data)
                 setError(null)
             } catch (err) {
@@ -34,4 +39,4 @@ const usePosts = (course = '', year = '') => {
     return { posts, loading, error }
 }
 
-export default usePosts
+export default usePosts 
