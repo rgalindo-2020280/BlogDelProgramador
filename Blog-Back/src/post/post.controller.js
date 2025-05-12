62d770db2f4618337aae3c57cc3f30bd17181e75
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-11 20:44:55 -0600

    Se agrega un getPostByYear

--- Blog-Back/src/post/post.controller.js
+++ Blog-Back/src/post/post.controller.js
@@ -3,20 +3,22 @@ import Post from './post.model.js'
 export const updatePost = async (req, res) => {
     try {
         const { id } = req.params
-        const { title, description, course } = req.body
-        const post = await Post.findByIdAndUpdate(id, { title, description, course }, { new: true })
+        const { title, description, course, year, projectLink } = req.body
 
-        if (!post) {
-            return res.status(404).send(
-                { 
-                    message: "Publicación no encontrada" 
-                }
-            )
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            { title, description, course, year, projectLink },
+            { new: true }
+        )
+
+        if (!updatedPost) {
+            return res.status(404).send({ message: "Publicación no encontrada" })
         }
-        res.status(200).send(post)
+
+        res.status(200).send(updatedPost)
     } catch (error) {
         res.status(400).send({
-            message: "Error al actualizar publicación",
+            message: "Error al actualizar la publicación",
             error
         })
     }
@@ -50,6 +52,27 @@ export const getAllPosts = async (req, res) => {
     } catch (error) {
         res.status(500).send({
                 message: "Error al obtener publicaciones",
+                error
+            }
+        )
+    }
+}
+
+export const getPostsByYear = async (req, res) => {
+    try {
+        const { year } = req.params
+        const posts = await Post.find({ year }).sort({ createdAt: -1 })
+        if (posts.length === 0) {
+            return res.status(404).send(
+                { 
+                    message: `No se encontraron publicaciones para el año: ${year}` 
+                }
+            )
+        }
+        res.status(200).send(posts)
+    } catch (error) {
+        res.status(500).send({
+                message: "Error al obtener publicaciones filtradas",
                 error
             }
         )
