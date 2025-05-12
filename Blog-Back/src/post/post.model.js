3f7346eb5fd03060070679232c3d48c8d39f55d2
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-11 20:42:39 -0600

    Se cambia el modelo de post

--- Blog-Back/src/post/post.model.js
+++ Blog-Back/src/post/post.model.js
@@ -1,25 +1,31 @@
 import { Schema, model } from "mongoose"
 
-
 const PostSchema = new Schema({
-        title: { 
-            type: String, 
-            required: true 
-        },
-        description: { 
-            type: String, 
-            required: true 
-        },
-        course: { 
-            type: String,
-            enum: ["Taller", "Tecnología", "Practica Supervisada"],
-            required: true 
-        },
-        createdAt: { 
-            type: Date, 
-            default: Date.now 
-        }
+    title: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    course: { 
+        type: String,
+        enum: ["Taller", "Tecnología", "Practica Supervisada"],
+        required: true 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+    year: { 
+        type: Number, 
+        required: true 
+    },
+    projectLink: { 
+        type: String,
+        required: true
     }
-)
+})
 
 export default model('Post', PostSchema)
