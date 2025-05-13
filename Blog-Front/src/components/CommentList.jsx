9a3294977b8b9f10a1aa49110d70f731a8169104
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-13 17:19:29 -0600

    Se agrega la fecha de publicacion de los comentarios

--- Blog-Front/src/components/CommentList.jsx
+++ Blog-Front/src/components/CommentList.jsx
@@ -7,6 +7,7 @@ const CommentList = ({ comments, onEditComment, onDeleteComment }) => {
                 <div key={comment._id} className="comment-item">
                     <h4>{comment.name}</h4>
                     <p>{comment.content}</p>
+                    <p><small>Publicado el: {new Date(comment.createdAt).toLocaleString()}</small></p> 
                     <button onClick={() => onEditComment(comment)}>Editar</button>
                     <button onClick={() => onDeleteComment(comment._id)}>Eliminar</button>
                 </div>
