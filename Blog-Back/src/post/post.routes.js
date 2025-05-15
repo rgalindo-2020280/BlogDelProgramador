03a812a2959cb7a2dec16225719ad2345fabf387
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-14 20:56:21 -0600

    Se agregan las validaciones a las rutas de post

--- Blog-Back/src/post/post.routes.js
+++ Blog-Back/src/post/post.routes.js
@@ -6,25 +6,30 @@ import {
     updatePost
 } from './post.controller.js'
 
+import { updatePostValidator, getPostsByCourseValidator, getPostsByYearValidator } from '../../helpers/validator.js'
+
 const api = Router()
 
 api.get(
     '/getPosts',
     getAllPosts
-)
+)/
 
 api.get(
     '/:course',
+    [getPostsByCourseValidator],
     getPostsByCourse
 )
 
 api.put(
     '/:id',
+    [updatePostValidator],
     updatePost
 )
 
 api.get(
     '/year/:year',
+    [getPostsByYearValidator],
     getPostsByYear
 )
 
