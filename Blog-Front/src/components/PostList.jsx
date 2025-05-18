e48c6d2402a488e7c8c124e07c53b180ba1d277b
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-05-17 21:05:07 -0600

    Se corrige un pequeño error

--- Blog-Front/src/components/PostList.jsx
+++ Blog-Front/src/components/PostList.jsx
@@ -7,38 +7,38 @@ const PostList = () => {
     const [year, setYear] = useState('')
     const { posts, loading, error } = usePosts(course, year)
 
-    const sortedPosts = posts.sort((a, b) => b.year - a.year);
-
     useEffect(() => {
-        console.log('Curso seleccionado:', course);
-        console.log('Año seleccionado:', year);
+        console.log('Curso seleccionado:', course)
+        console.log('Año seleccionado:', year)
     }, [course, year])
-    
-    return (
-        <div className="container">
-            <select onChange={(e) => setCourse(e.target.value)} value={course}>
-                <option value="">Todos los cursos</option>
-                <option value="Taller">Taller</option>
-                <option value="Tecnología">Tecnología</option>
-                <option value="Practica Supervisada">Práctica Supervisada</option>
-            </select>
 
-            <select onChange={(e) => setYear(e.target.value)} value={year}>
-                <option value="">Todos los Años</option>
-                <option value="2023">2023</option>
-                <option value="2024">2024</option>
-                <option value="2025">2025</option>
-            </select>
+  return (
+    <div className="container">
+        <select onChange={(e) => setCourse(e.target.value)} value={course}>
+            <option value="">Todos los cursos</option>
+            <option value="Taller">Taller</option>
+            <option value="Tecnología">Tecnología</option>
+            <option value="Practica Supervisada">Práctica Supervisada</option>
+        </select>
 
-            {loading && <p>Cargando...</p>}
-            {error && <p>{error}</p>}
+        <select onChange={(e) => setYear(e.target.value)} value={year}>
+            <option value="">Todos los Años</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+        </select>
 
-            {posts.length > 0 ? (
-                posts.map((post) => <PostCard key={post._id} post={post} />)
-            ) : (
-                <p>No se encontraron publicaciones para el filtro seleccionado</p>
-            )}
-        </div>
+        {loading && <p>Cargando...</p>}
+        {error && <p>{error}</p>}
+
+        {!loading && !error && Array.isArray(posts) && posts.length > 0 ? (
+            posts.map((post) => <PostCard key={post._id} post={post} />)
+        ) : null}
+
+        {!loading && !error && Array.isArray(posts) && posts.length === 0 && (
+            <p>No se encontraron publicaciones para el filtro seleccionado</p>
+        )}
+    </div>
     )
 }
 
